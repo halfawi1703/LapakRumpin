1a7d8df189e66f923888e30a56bdf4af2dfccdf1
halfawi1703 <halfawi1703@gmail.com>
2023-07-25 23:20:08 +0700

    fixing menu service

--- application/views/templates/navbar.php
+++ application/views/templates/navbar.php
@@ -10,8 +10,8 @@
             <ul>
                 <li><a class="nav-link scrollto active" href="#hero">Home</a></li>
                 <li><a class="nav-link scrollto" href="#about">About</a></li>
-                <li><a class="nav-link scrollto" href="#services">Services</a></li>
-                <li><a class="nav-link   scrollto" href="#portfolio">Portfolio</a></li>
+                <li><a class="nav-link scrollto" href="#why-us">Services</a></li>
+                <li><a class="nav-link scrollto" href="#portfolio">Portfolio</a></li>
                 <li><a class="getstarted scrollto" href="#contact">Hubungi</a></li>
             </ul>
             <i class="bi bi-list mobile-nav-toggle"></i>

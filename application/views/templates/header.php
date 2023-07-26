6e583ec7faaf1070996fda900f6a33bc8b1f85ce
halfawi1703 <halfawi1703@gmail.com>
2023-07-26 14:43:24 +0700

    google analytics

--- application/views/templates/header.php
+++ application/views/templates/header.php
@@ -2,39 +2,52 @@
 <html lang="en">
 
 <head>
-    <meta charset="utf-8">
-    <meta content="width=device-width, initial-scale=1.0" name="viewport">
+  <meta charset="utf-8">
+  <meta content="width=device-width, initial-scale=1.0" name="viewport">
 
-    <title>LapakRumpin</title>
-    <meta content="" name="description">
-    <meta content="" name="keywords">
+  <title>LapakRumpin</title>
+  <meta content="" name="description">
+  <meta content="" name="keywords">
 
-    <!-- Favicons -->
-    <link href="<?php echo base_url('assets') ?>/img/favicon.ico" rel="icon">
-    <link href="<?php echo base_url('assets') ?>/img/Logo-v1.png" rel="Logo-v1">
+  <!-- Favicons -->
+  <link href="<?php echo base_url('assets') ?>/img/favicon.ico" rel="icon">
+  <link href="<?php echo base_url('assets') ?>/img/Logo-v1.png" rel="Logo-v1">
 
-    <!-- Google Fonts -->
-    <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,300i,400,400i,600,600i,700,700i|Jost:300,300i,400,400i,500,500i,600,600i,700,700i|Poppins:300,300i,400,400i,500,500i,600,600i,700,700i" rel="stylesheet">
+  <!-- Google Fonts -->
+  <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,300i,400,400i,600,600i,700,700i|Jost:300,300i,400,400i,500,500i,600,600i,700,700i|Poppins:300,300i,400,400i,500,500i,600,600i,700,700i" rel="stylesheet">
 
-    <!-- Vendor CSS Files -->
-    <link href="<?php echo base_url('assets') ?>/vendor/aos/aos.css" rel="stylesheet">
-    <link href="<?php echo base_url('assets') ?>/vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
-    <link href="<?php echo base_url('assets') ?>/vendor/bootstrap-icons/bootstrap-icons.css" rel="stylesheet">
-    <link href="<?php echo base_url('assets') ?>/vendor/boxicons/css/boxicons.min.css" rel="stylesheet">
-    <link href="<?php echo base_url('assets') ?>/vendor/glightbox/css/glightbox.min.css" rel="stylesheet">
-    <link href="<?php echo base_url('assets') ?>/vendor/remixicon/remixicon.css" rel="stylesheet">
-    <link href="<?php echo base_url('assets') ?>/vendor/swiper/swiper-bundle.min.css" rel="stylesheet">
+  <!-- Vendor CSS Files -->
+  <link href="<?php echo base_url('assets') ?>/vendor/aos/aos.css" rel="stylesheet">
+  <link href="<?php echo base_url('assets') ?>/vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+  <link href="<?php echo base_url('assets') ?>/vendor/bootstrap-icons/bootstrap-icons.css" rel="stylesheet">
+  <link href="<?php echo base_url('assets') ?>/vendor/boxicons/css/boxicons.min.css" rel="stylesheet">
+  <link href="<?php echo base_url('assets') ?>/vendor/glightbox/css/glightbox.min.css" rel="stylesheet">
+  <link href="<?php echo base_url('assets') ?>/vendor/remixicon/remixicon.css" rel="stylesheet">
+  <link href="<?php echo base_url('assets') ?>/vendor/swiper/swiper-bundle.min.css" rel="stylesheet">
 
-    <!-- Template Main CSS File -->
-    <link href="<?php echo base_url('assets') ?>/css/style.css" rel="stylesheet">
+  <!-- Template Main CSS File -->
+  <link href="<?php echo base_url('assets') ?>/css/style.css" rel="stylesheet">
 
-    <!-- =======================================================
+  <!-- =======================================================
   * Template Name: Arsha
   * Updated: Jul 05 2023 with Bootstrap v5.3.0
   * Template URL: https://bootstrapmade.com/arsha-free-bootstrap-html-template-corporate/
   * Author: BootstrapMade.com
   * License: https://bootstrapmade.com/license/
   ======================================================== -->
+
+  <!-- Google tag (gtag.js) -->
+  <script async src="https://www.googletagmanager.com/gtag/js?id=G-D44WWJ0L4N"></script>
+  <script>
+    window.dataLayer = window.dataLayer || [];
+
+    function gtag() {
+      dataLayer.push(arguments);
+    }
+    gtag('js', new Date());
+
+    gtag('config', 'G-D44WWJ0L4N');
+  </script>
 </head>
 
 <body>

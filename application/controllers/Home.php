411e24c9496f8df855051881066bc63774f0b983
halfawi1703 <halfawi1703@gmail.com>
2024-06-27 00:24:06 +0700

    next commit

--- application/controllers/Home.php
+++ application/controllers/Home.php
@@ -1,15 +1,16 @@
 <?php
-defined('BASEPATH') OR exit('No direct script access allowed');
+defined('BASEPATH') or exit('No direct script access allowed');
 
-class Home extends CI_Controller {
+class Home extends CI_Controller
+{
 
-	public function index()
-	{
-        $data = [];
-        
-        $this->load->view('templates/header', $data);
-        $this->load->view('templates/navbar', $data);
-        $this->load->view('modules/home/index', $data);
-        $this->load->view('templates/footer', $data);
-	}
+        public function index()
+        {
+                $data = [];
+
+                $this->load->view('templates/header', $data);
+                $this->load->view('templates/navbar', $data);
+                $this->load->view('modules/home/index', $data);
+                $this->load->view('templates/footer', $data);
+        }
 }
